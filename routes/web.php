867e5f9114b55bf9e2166771bb69fe11d8e496ec
roni <roniellipedrocontato@gmail.com>
2025-06-15 16:13:54 -0300

    adicionando tela de login/register adaptando as rotas

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,21 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 
-Route::prefix('painel')->group(function () {
-    Route::get('/', [AdminController::class, 'index']);
+Route::get('/painel', [AdminController::class, 'index'])->middleware(['auth', 'verified'])->name('painel');
+
+// Route::prefix('painel')->group(function () {
+//     Route::get('/', [AdminController::class, 'index']);
+// });
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__ . '/auth.php';
