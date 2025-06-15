8c40d59f570c631dee52f65a188a62a1aa479cfb
roni <roniellipedrocontato@gmail.com>
2025-06-14 22:28:33 -0300

    configurando as rotas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index']);
+
+Route::prefix('painel')->group(function () {
+    Route::get('/', [AdminController::class, 'index']);
 });
