121d7d74589b27c63b41eb6ba4ce23bad0e45bbc
roni <roniellipedrocontato@gmail.com>
2025-06-16 00:32:44 -0300

    configurando adminlte

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,1 +1,21 @@
-ADMIN
+@extends('adminlte::page')
+
+@section('title', 'Painel')
+
+@section('content_header')
+    <h1>Painel de Controle</h1>
+@endsection
+
+@section('content')
+    ...
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="/assets/css/custom.css" />
+@endsection
+
+@section('js')
+    <script>
+        alert("RODANDO!");
+    </script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,6 @@ Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/painel', [AdminController::class, 'index'])->middleware(['auth', 'verified'])->name('painel');
 
-// Route::prefix('painel')->group(function () {
-//     Route::get('/', [AdminController::class, 'index']);
-// });
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
