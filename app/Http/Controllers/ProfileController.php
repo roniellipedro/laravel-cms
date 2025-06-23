ba1e2682b4cd5aa346adb6707815914eda4744ed
roni <roniellipedrocontato@gmail.com>
2025-06-23 07:04:13 -0300

    configurando controller do profile para o usuario editar seu proprio perfil

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -14,11 +15,18 @@ class ProfileController extends Controller
     /**
      * Display the user's profile form.
      */
-    public function edit(Request $request): View
+    public function edit(Request $request)
     {
-        return view('profile.edit', [
-            'user' => $request->user(),
-        ]);
+
+        $loggedId = Auth::id();
+
+        $user = User::find($loggedId);
+
+        if ($user) {
+            return view('admin.profile.index', ['user' => $user]);
+        }
+
+        return redirect()->route('painel');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,29 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 // use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 
-Route::middleware('auth')->group(function () {
-    Route::delete('/painel/users/destroy/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-    Route::put('/painel/users/update/{id}', [UserController::class, 'update'])->name('users.update');
-    Route::post('/painel/users/store', [UserController::class, 'store'])->name('users.store');
-    Route::get('/painel/users/create', [UserController::class, 'create'])->name('users.create');
-    Route::get('/painel/users/edit/{id}', [UserController::class, 'edit'])->name('users.edit');
-    Route::get('/painel', [AdminController::class, 'index'])->name('painel');
+Route::middleware('auth')->prefix('painel')->group(function () {
+    Route::delete('/users/destroy/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::put('/users/update/{id}', [UserController::class, 'update'])->name('users.update');
+    Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::get('/users/edit/{id}', [UserController::class, 'edit'])->name('users.edit');
+    Route::get('', [AdminController::class, 'index'])->name('painel');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    
     // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::middleware('can:edit-users')->group(function () {
-    Route::get('/painel/users', [UserController::class, 'index'])->name('painel.users');
+Route::middleware('can:edit-users')->prefix('painel')->group(function () {
+    Route::get('/users', [UserController::class, 'index'])->name('painel.users');
 });
 
 require __DIR__ . '/auth.php';
