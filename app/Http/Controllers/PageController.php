8f90b766ec68430a7a5fed2192a1aecc82b65f82
roni <roniellipedrocontato@gmail.com>
2025-06-24 07:03:30 -0300

    Page - iniciando a criação do HTML da página de criar

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -22,7 +22,7 @@ class PageController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.pages.create');
     }
 
     /**
---
+++ resources/views/admin/pages/create.blade.php
@@ -0,0 +1,62 @@
+@extends('adminlte::page')
+
+@section('title', 'Nova Página')
+
+@section('content_header')
+    <h1>
+        Nova Página
+    </h1>
+@endsection
+
+@section('content')
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <div class="mb-0">
+                @foreach ($errors->all() as $error)
+                    <i class="icon fas fa-ban"></i>
+                    <span>{{ $error }}</span>
+                    <br>
+                @endforeach
+            </div>
+        </div>
+
+    @endif
+
+    <div class="card">
+        <form action="{{ route('pages.store') }}" method="POST" class="form-horizontal">
+            @csrf
+            <div class="card-body">
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">
+                        Título
+                    </label>
+                    <div class="col-sm-10">
+                        <input type="text" name="name" value="{{ old('title') }}"
+                            class="form-control @error('title') is-invalid @enderror">
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">
+                        Corpo
+                    </label>
+                    <div class="col-sm-10">
+
+                        <textarea name="body" class="form-control">{{ old('body') }}</textarea>
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">
+                    </label>
+                    <div class="col-sm-6 ">
+                        <input type="submit" value="Criar" class="btn btn-success ">
+                    </div>
+                </div>
+
+            </div>
+        </form>
+    </div>
+
+@endsection
