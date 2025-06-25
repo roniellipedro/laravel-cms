2271cc52cf4b6e5ef538452417cc5159cd7aa5b0
roni <roniellipedrocontato@gmail.com>
2025-06-25 06:47:03 -0300

    dashboard/admin - adicionando valores nos contadores de forma dinâmica

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Page;
+use App\Models\User;
+use App\Models\Visitor;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -14,6 +17,18 @@ class AdminController extends Controller
 
     public function index()
     {
-        return view('admin.home');
+        $data['visitsCount'] = Visitor::count();
+
+        $datelimit = date('Y-m-s H:i:s', strtotime('-5 minutes'));
+        $dateList = Visitor::select('ip')->where('date_access', '>=', $datelimit)->groupBy('ip')->get();
+        $data['onlineCount'] = count($dateList);
+
+
+        $data['pageCount'] = Page::count();
+
+        $data['userCount'] = User::count();
+
+
+        return view('admin.home', $data);
     }
 }
